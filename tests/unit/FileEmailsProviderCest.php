a955c85e4c85708ac5914990efe08afdcee1c9b3
Oleg Kostiushko <o.kostyshko@gmail.com>
2019-08-29 19:43:13 +0300

    got test but no implementation

--- tests/unit/FileEmailsProviderCest.php
+++ tests/unit/FileEmailsProviderCest.php
@@ -6,13 +6,21 @@ class FileEmailsProviderCest
     const TEST_FILE_PATH = __DIR__ . '/../_output/test_subscription_list.txt';
 
 
-    public function testFetchAll()
+    public function testFetchAll(UnitTester $I)
     {
+        file_put_contents(self::TEST_FILE_PATH, 'test@qa.mail' . PHP_EOL . 'test_2@qae', FILE_TEXT);
+        $provider = new \Subscription\FileEmailsProvider(self::TEST_FILE_PATH);
 
+        $I->assertSame(['test@qa.mail', 'test_2@qae'], $provider->fetchAll());
     }
 
-    public function testAppendMany()
+    public function testAppendMany(UnitTester $I)
     {
+        file_put_contents(self::TEST_FILE_PATH, 'test@qa.mail' . PHP_EOL . 'test_2@qae', FILE_TEXT);
+        $provider = new \Subscription\FileEmailsProvider(self::TEST_FILE_PATH);
 
+        $provider->appendMany(['test3@qa.mail', 'test4@qa.mail']);
+
+        $I->assertSame(['test@qa.mail', 'test_2@qae', 'test3@qa.mail', 'test4@qa.mail'], $provider->fetchAll());
     }
 }
