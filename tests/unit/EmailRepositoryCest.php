4ea58b1cac18f68d3e56f092c0fad6787c77f8ab
Oleg Kostiushko <o.kostyshko@gmail.com>
2019-08-29 19:24:11 +0300

    write test but them failed

---
+++ tests/unit/EmailRepositoryCest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+use Subscription\Exceptions\EmailAlreadyExistsException;
+use Tokenly\TokenGenerator\TokenGenerator;
+
+class EmailRepositoryCest
+{
+
+    const TEST_FILE_PATH = __DIR__ . '/../_output/test_subscription_list.txt';
+
+    /**
+     * @var \Subscription\EmailRepository
+     */
+    private $storage;
+
+    public function _before(UnitTester $I)
+    {
+        file_put_contents(self::TEST_FILE_PATH, '', FILE_TEXT);
+        $this->storage = new \Subscription\EmailRepository(self::TEST_FILE_PATH);
+    }
+
+    // tests
+    public function testNotExists(UnitTester $I)
+    {
+        $I->assertFalse($this->storage->exists('some@test.email'));
+    }
+
+    public function testExists(UnitTester $I)
+    {
+        file_put_contents(self::TEST_FILE_PATH, 'some@test.email' . PHP_EOL, FILE_TEXT);
+        $I->assertTrue($this->storage->exists('some@test.email'));
+    }
+
+    public function testAdd(UnitTester $I)
+    {
+        $email = $I->generateRandomEmail();
+
+        $this->storage->persist($email);
+        $this->storage->flush();
+
+        $I->assertTrue($this->storage->exists($email));
+
+        $I->expectException(EmailAlreadyExistsException::class, function () use ($email) {
+            $this->storage->persist($email);
+        });
+    }
+}
